a2c487b02688987c91ed944df583994d53855822
salman76code <salmanhussainkh444@gmail.com>
2024-11-27 12:04:22 +0300

    v2.0

--- Glow_event/ViewController.swift
+++ Glow_event/ViewController.swift
@@ -10,20 +10,27 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var event_photo: UIButton!
+    @IBOutlet weak var start_dtpicker: UIDatePicker!
+
     
     @IBAction func Button(_ sender: UIButton) {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        event_photo.layer.cornerRadius = 5
+        start_dtpicker.setValue(UIColor.white, forKey: "textColor")
         
-        event_photo.layer.cornerRadius = 25
+        
+        // Set the tint color to white
+       
+        
     }
+    
    
     
-    
-
-
   
 }
+
 
