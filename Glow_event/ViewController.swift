f13f01c3b04da28b88bab7038aeeeb34ef6803c0
Zainab <zainab@Zainabs-MacBook-Pro.local>
2024-12-07 20:07:17 +0300

    Commit

--- Glow_event/ViewController.swift
+++ Glow_event/ViewController.swift
@@ -4,20 +4,14 @@
 //
 //  Created by BP-36-201-09 on 25/11/2024.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var event_photo: UIButton!
-    
-    @IBAction func Button(_ sender: UIButton) {
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        event_photo.layer.cornerRadius = 25
     }
    
     
