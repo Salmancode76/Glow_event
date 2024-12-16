a7f0a006f3675b376c4f740970f753fe57af1759
Salman76code <salmanhussainkh444@gmail.com>
2024-12-16 23:24:11 +0300

    v16.0

--- Glow_event/FilterTableViewController.swift
+++ Glow_event/FilterTableViewController.swift
@@ -18,26 +18,51 @@ class FilterTableViewController: UITableViewController {
     var selectedAge:String = "Any"
     var selectedVenu : String = "Any"
     var selectedStatus:String = "Any"
+    var selectedSort : String = "Any"
     var events: [Event] = [] // Events passed from SearchEventOrgViewController
     var filteredEvents: [Event] = []
     var FromDate : Date?
     var ToDate : Date?
-    @IBOutlet weak var ToEventPKR: UIDatePicker!
     @IBOutlet weak var EventCatgory: UIButton!
     @IBOutlet weak var EventAge: UIButton!
     @IBOutlet weak var EventVen: UIButton!
     @IBOutlet weak var EventStatusBtn: UIButton!
     @IBOutlet weak var FromEventPKR: UIDatePicker!
+    @IBOutlet weak var EventSortPriceBtn: UIButton!
+    @IBOutlet weak var ToEventPKR: UIDatePicker!
+
     
     weak var delegate: FilterTableViewControllerDelegate? // Delegate reference
     
+    @IBAction func OptionSortPrice(_ sender: UIAction) {
+        self.selectedSort = sender.title
+        
+        self.EventSortPriceBtn.setTitle(selectedSort, for: .normal)
+        
+    }
     
+    @IBAction func OptionFilterCategory(_ sender: UIAction) {
+        // Directly access sender.title since it's a non-optional String
+        self.selectedEventCategory = sender.title
+        self.EventCatgory.setTitle(selectedEventCategory, for: .normal)
+        
+        print("Filtered events: ", self.selectedStatus)
+    }
     @IBAction func OptionFilterStatus(_ sender: UIAction) {
         // Directly access sender.title since it's a non-optional String
         self.selectedStatus = sender.title
         self.EventStatusBtn.setTitle(selectedStatus, for: .normal)
         
         print("Filtered events: ", self.selectedStatus)
+    }
+    
+    @IBAction func OptionFilterDateFrom(_ sender: UIDatePicker) {
+        // Directly access sender.title since it's a non-optional String
+        self.FromDate = sender.date
+    }
+    @IBAction func OptionFilterDateTo(_ sender: UIDatePicker) {
+        // Directly access sender.title since it's a non-optional String
+        self.ToDate = sender.date
     }
 
 
@@ -67,13 +92,11 @@ class FilterTableViewController: UITableViewController {
         // Apply the date range filter based on switch state
         var filteredByDate: [Event] = filteredByCategory
 
-        if AnyDateSW.isOn {
-            // If the switch is ON, we don't use the date range for filtering
-            print("Date filtering is disabled because the switch is ON")
-        } else {
+        if !AnyDateSW.isOn {
             // If the switch is OFF, use the date range for filtering
             guard let fromDate = self.FromDate, let toDate = self.ToDate else {
-                print("Invalid FromDate or ToDate")
+                // Either FromDate or ToDate is nil, so we return early or handle it
+                print("FromDate or ToDate is nil")
                 return
             }
 
@@ -102,12 +125,27 @@ class FilterTableViewController: UITableViewController {
                 return event.EventStatus.lowercased() == selectedStatus.lowercased()
             }
         }
+        
+        let Sorted : [Event]
+        
+        print(selectedSort)
+     
+         if selectedSort == "Highest"{
+            Sorted = filteredByStatus.sorted { event1, event2 in
+                   return event2.price > event1.price
+               }
+         }else if selectedSort == "Lowest"{
+             Sorted = filteredByStatus.sorted { event1, event2 in
+                    return event2.price < event1.price
+                }
+         }else{
+             Sorted = filteredByStatus
+         }
 
         // Print filtered events for debugging purposes
-        print("Filtered events: ", filteredByStatus)
 
         // Pass the filtered events to the delegate
-        delegate?.applyFilterWith(events: filteredByStatus)
+        delegate?.applyFilterWith(events: Sorted)
 
         // Pop back to the previous view controller
         navigationController?.popViewController(animated: true)
@@ -115,13 +153,62 @@ class FilterTableViewController: UITableViewController {
 
 
 
+    @IBAction func ResetFilters(_ sender: Any) {
 
+           AnyDateSW.setOn(true, animated: true)
+           
+     
+           FromEventPKR.date = Date()
+           ToEventPKR.date = Date()
+           FromDate = nil
+           ToDate = nil
+           
+      
+           EventCatgory.setTitle("Any", for: .normal)
+           EventAge.setTitle("Any", for: .normal)
+           EventVen.setTitle("Any", for: .normal)
+           EventStatusBtn.setTitle("Any", for: .normal)
+           EventSortPriceBtn.setTitle("Any", for: .normal)
+           
+          
+           selectedEventCategory = "Any"
+           selectedAge = "Any"
+           selectedVenu = "Any"
+           selectedStatus = "Any"
+           selectedSort = "Any"
+           
+        
+           filteredEvents = events
+           
+      
+           tableView.reloadData()
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+     
         
 
         
+        FromEventPKR.backgroundColor = UIColor.black
         
+        
+        ToEventPKR.backgroundColor = UIColor.black
+        
+        
+        FromEventPKR.tintColor = UIColor.white
+        ToEventPKR.tintColor = UIColor.black
+        
+        // Customize section header color
+        // MARK: - TableView Delegate Method to Customize Header Appearance
+  
+        
+        
+        if #available(iOS 14.0, *) {
+            FromEventPKR.setValue(UIColor.white, forKeyPath: "textColor")
+            ToEventPKR.setValue(UIColor.white, forKeyPath: "textColor")
+        }
         
         func numberOfSections(in tableView: UITableView) -> Int {
             // #warning Incomplete implementation, return the number of sections
@@ -137,6 +224,23 @@ class FilterTableViewController: UITableViewController {
         print(events);
 
     }
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+       guard let header = view as? UITableViewHeaderFooterView else { return }
+       
+       // Set the background color of the header to white
+       header.contentView.backgroundColor = .black
+       
+       // Set the text color of the header to black
+       header.textLabel?.textColor = .white
+       
+        tableView.tableFooterView = UIView(frame: .zero)
+        
+        // Set the separator color to gray
+        tableView.separatorColor = UIColor.white
+       //tableView.separatorStyle = .none
+
+  
+   }
     
     
     @IBAction func AnyDateSwitchChanged(_ sender: UISwitch) {
@@ -147,6 +251,8 @@ class FilterTableViewController: UITableViewController {
       // MARK: - Helper Methods
       private func toggleDatePickersState() {
           // Disable date pickers if switch is OFF, enable if switch is ON
+          
+          print("pressed")
           let isSwitchOff = !AnyDateSW.isOn
           FromEventPKR.isEnabled = isSwitchOff
           ToEventPKR.isEnabled = isSwitchOff
