bc97b904607c5e4c9137efc376bf9f7de5a789c0
sharmin011 <sharminah.011@gmail.com>
2024-12-26 22:28:25 +0300

    comm

--- Glow_event/AppDelegate.swift
+++ Glow_event/AppDelegate.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import Firebase
 
 
 @main
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+        FirebaseApp.configure()
+        print("Firebase is configured!")
         
         // Override point for customization after application launch.
         return true
--- Pods/Target Support Files/Pods-Glow_event/Pods-Glow_event-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Glow_event : NSObject
-@end
-@implementation PodsDummy_Pods_Glow_event
-@end
